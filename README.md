391a86cdd6de4d46bd4cf03a30714d0a5bb0900c
lsamano <leizl.samano@flatironschool.com>
2019-10-07 16:52:37 -0400

    first deliverables

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# employee-tracker-lab
+# Employee Tracker Lab
+You've been hired as a frontend developer for a startup company. They would like you to build the frontend of a single-page app (SPA) to help them keep track of their employees.
+
+# Setup
+Run `json-server db.json --watch --port 3003` to run your backend server on port 3003 (feel free to change your chosen port).
+
+# Deliverables
+### A. Viewing All Employees
+1. On page load, show the list of employees by *full name* in the `sidebar` div. It should look like `LastName, FirstName`.
+2. When an employee name is clicked, all of their info should be shown in the `info-box` div. This includes:
+    - Their avatar
+    - Their full name
+    - Their job title
+    - Their job description
+    - Their EIN (Employee Identification Number)
+    - The date they started with the company
+Be sure to look over the data structure in the json file.
+### B. Adding New Employees
+Nice work! Your manager is impressed. You're now tasked with adding the ability to add newly-hired employees to the Employee Tracker.
+3. When I click a link on the top navbar, the `info-box` changes to a form.
+4. Submission of this form adds the new employee to the database.
+5. After the employee is successfully added to the database, 
+
+Keep the names in the sidebar in order of last name.
+Have selected name light up as active.
---
+++ index.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+    runTheFetch()
+})
+
+runTheFetch = () => {
+    return fetch('http://localhost:3003/employees')
+        .then(data => data.json())
+        .then(data => {
+            orderedData = data.sort((a, b) => a.lastName.localeCompare(b.lastName))
+            printTheStuff(orderedData)
+        })
+}
+
+// Run a function to print the stuff
+function printTheStuff(info) {
+    info.forEach(forEachObject)
+}
+
+const forEachObject = object => {
+    // Get the navbar
+    const theNavbar = document.getElementById("employee-list")
+    const infoBox = document.getElementById("employee-info")
+
+    // Make the li with the text
+    const oneLi = document.createElement('a')
+    oneLi.className = 'collection-item'
+    oneLi.innerText = `${object.lastName}, ${object.firstName}`
+
+    // Add click event
+    oneLi.addEventListener('click', event => {
+        
+        infoBox.innerHTML = `
+        <img src="${object.avatar}" alt="avatar of ${object.firstName} ${object.lastName}">
+        <h1>${object.firstName} ${object.lastName}</h1>
+        <h5>${object.job.title}</h5>
+        <p>${object.job.description}</p>
+        `
+    })
+
+    // Add the li to the navbar
+    theNavbar.append(oneLi)
+}
+
+// makeFormFunctional = () => {
+//     const newForm = document.getElementById('new-form')
+
+//     newForm.addEventListener('submit', event => {
+//         event.preventDefault()
+
+//         /////////////////////////
+//         const newObject = {
+//             word: newForm.word.value,
+//             moreInfo: newForm.info.value
+//         }
+//         forEachObject(newObject)
+//         /////////////////////////
+//         fetch('http://localhost:3000/datas', {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json'
+//             },
+//             body: JSON.stringify(newObject)
+//         })
+//         // .then(data => data.json())
+
+
+//         newForm.reset()
+//     })
+// }
